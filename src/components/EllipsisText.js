4d1b375a684f131ce5f954764c66617e9ee47340
pd-smith <psmithdb@gmail.com>
2019-08-27 21:34:20 -0400

    Some component cleanup

--- src/components/EllipsisText.js
+++ src/components/EllipsisText.js
@@ -1,55 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const styles = {
-  allText: {
-    MozUserSelect: 'text',
-    WebkitUserSelect: 'text',
-    msUserSelect: 'text',
-    userSelect: 'text'
-  },
-}
-
-class EllipsisText extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
-  render() {
-    let {
-      text,
-      length,
-      tail,
-      tailClassName,
-      ...others
-    } = this.props;
-
-    if (text.length <= this.props.length || this.props.length < 0) {
-      return (<span {...others}>{this.props.text}</span>);
-    } else {
-
-      const tailStyle = {
-        cursor: 'auto'
-      };
-
-      let displayText;
-      if (length - tail.length <= 0){
-        displayText = '';
-      } else {
-        displayText = text.slice(0, (length - tail.length))
-      }
-
-      return (
-        <span title={this.props.text} {...others}>
-          {displayText}
-          <span style={tailStyle}
-                className={tailClassName}>
+const EllipsisText = ({
+    text,
+    length,
+    tail,
+    tailClassName,
+    ...others
+}) => {
+  const lengthOfVisibleText = length - tail.length; 
+  return (
+    <span title={text} {...others}>
+      {(text.length <= length || length < 0) ? text : (
+        <>
+          {lengthOfVisibleText > 0 && text.slice(0, lengthOfVisibleText)}
+          <span
+              className={tailClassName}
+              style={{ cursor: 'auto'}}>
             {tail}
           </span>
-        </span>);
-    }
-  }
+        </>
+      )}
+    </span>
+  );
 }
 
 EllipsisText.propTypes = {
